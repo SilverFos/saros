0054cd85185fc0d93c4b1541eeaf1776cc8ab2cd
SilverFos <sanalvsolomon@gmail.com>
2026-08-16 17:00:59 +0530

    Added separation of 802.3 and Ethernet_II frames.

--- parse.cpp
+++ parse.cpp
@@ -56,6 +56,11 @@ int main()
 		printf("EtherType: 0x%04X\n", frame.ethType);
 		printf("Destination MAC: %02X:%02x:%02X:%02X:%02X:%02X \n", frame.destAdd[0], frame.destAdd[1], frame.destAdd[2], frame.destAdd[3], frame.destAdd[4], frame.destAdd[5]);
 		printf("Source MAC: %02X:%02x:%02X:%02X:%02X:%02X \n", frame.srcAdd[0], frame.srcAdd[1], frame.srcAdd[2], frame.srcAdd[3], frame.srcAdd[4], frame.srcAdd[5]);
+		if(frame.ethType>=1536){
+			printf("Ethernet II (DIX) Protocol: %04X\n", frame.ethType);
+		}else{
+			printf("IEEE 802.3 frame. Size: %d\n", frame.ethType);
+		}
 	}
 	return 0;	
 }
